f494854fe5bec82d222d127166c3df738fc9a783
Jeki Gates <jekigates2004@gmail.com>
2022-05-23 23:10:21 +0700

    finish code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <h1 id="teks1"></h1>
+      <h1 id="teks2"></h1>
+      <h1 id="teks3"></h1>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data: function () {
+    return {
+      kalimat1: "Hi, Nama!",
+      kalimat2: "Ini aku, Nama. Aku mau liatin video bermakna.",
+      kalimat3: "Tolong dilihat sampai habis ya.",
+      i1: 0,
+      i2: 0,
+      i3: 0,
+    };
+  },
+  mounted: function () {
+    this.ubahKalimat1();
+  },
+  methods: {
+    ubahKalimat1() {
+      const timer1 = setInterval(() => {
+        if (this.i1 < this.kalimat1.length) {
+          document.getElementById("teks1").innerHTML += this.kalimat1[this.i1];
+          this.i1++;
+        } else {
+          clearInterval(timer1);
+          this.ubahKalimat2();
+        }
+      }, 100);
+    },
+    ubahKalimat2() {
+      const timer2 = setInterval(() => {
+        if (this.i2 < this.kalimat2.length) {
+          document.getElementById("teks2").innerHTML += this.kalimat2[this.i2];
+          this.i2++;
+        } else {
+          clearInterval(timer2);
+          this.ubahKalimat3();
+        }
+      }, 100);
+    },
+    ubahKalimat3() {
+      const timer3 = setInterval(() => {
+        if (this.i3 < this.kalimat3.length) {
+          document.getElementById("teks3").innerHTML += this.kalimat3[this.i3];
+          this.i3++;
+        } else {
+          clearInterval(timer3);
+          this.playRickroll();
+        }
+      }, 100);
+    },
+    playRickroll() {
+      setTimeout(() => {
+        location.href = "https://youtu.be/dQw4w9WgXcQ";
+      }, 3000);
+    },
+  },
+};
 </script>
 
 <style>
